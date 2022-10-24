bb9ba5cb0a4b1216ac9356da08b9d5068e00bb3f
Атнагулов Илья <114065510+IlAt228@users.noreply.github.com>
2022-10-24 22:27:14 +0300

    Update Lab. №1 Part 2 (for).jl

--- Labs/Lab. №1 Part 2 (for).jl
+++ Labs/Lab. №1 Part 2 (for).jl
@@ -2,17 +2,14 @@
 #Вариант 2
 print("Привет, эта программа определяет количество нечётных цифр в твоём числе!
 Напиши своё число: ")
-i = parse(Int128,readline())
+num = parse(Int128,readline())
 d = 0
-for j = 1:10000
-    if i < 1
-        break
-    else 
-        z = i % 2
-        if z == 1
-             global d += 1
-        end
+for i in range(1,length(string(num)))
+    z = num % 2
+    if z == 1
+        global d += 1
     end
-    global i = div(i,10)
+
+    global num = div(num,10)
 end
 println(d)
